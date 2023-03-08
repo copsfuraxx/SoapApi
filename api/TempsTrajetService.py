106071960f79cc86e36af3e3da266915032c9ba1
Jules Finck <jules.finck@hotmail.fr>
2023-03-08 12:48:19 +0100

    v0.2

--- api/TempsTrajetService.py
+++ api/TempsTrajetService.py
@@ -1,26 +1,20 @@
 from spyne import Application, rpc, ServiceBase
-from spyne import Array
 from spyne.protocol.soap import Soap11
 from spyne.server.wsgi import WsgiApplication
 from wsgiref.simple_server import make_server
-import math
 
 class TempsTrajetService(ServiceBase):
     
-    @rpc(Array(Array(float)),float, _returns=float)
-    def tempsMoy(ctx, bornes, vitMoy):
+    @rpc(float,float,float,int, _returns=float)
+    def tempsMoy(ctx, distKm, vitMoyKMH, rechargeH, nb_recharge):
         ctx.transport.resp_headers['Access-Control-Allow-Origin'] = '*'
-        res = 0
-        last = bornes[0]
-        for i in bornes:
-            res += math.sqrt((i[0] - last[0]) ** 2 + (i[1] - last[1]) ** 2)
-        return res / 1000 / vitMoy
+        temps = distKm / vitMoyKMH
+        temps += rechargeH * nb_recharge
+        return temps
 
 application = Application([TempsTrajetService], 'spyne.examples.trajet.soap',
 in_protocol=Soap11(validator='lxml'),
 out_protocol=Soap11())
 app = WsgiApplication(application)
-
-#if __name__ == '__main__':
-#    server = make_server('127.0.0.1', 8000, wsgi_application)
-#    server.serve_forever()
+#server = make_server('localhost', 8000, app)
+#server.serve_forever()
